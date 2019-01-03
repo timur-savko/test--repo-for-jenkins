51f21465cf77d26a0e00626fc94e047568d57600
Timur <timur.savko@gmail.com>
2019-01-03 15:07:59 +0300

    Update HomePageTest.java

--- src/test/java/by/bsu/pagefactoryselenium/HomePageTest.java
+++ src/test/java/by/bsu/pagefactoryselenium/HomePageTest.java
@@ -26,6 +26,14 @@ public class HomePageTest {
     @Test
     public void shouldSuggestedTextChangedToEnglishLangWhenEnglishClicked() {
         HomePage action = PageFactory.initElements(driver, HomePage.class);
+          try
+  {
+      Thread.sleep(5000);
+  }
+  catch (InterruptedException e)
+  {
+       Thread.currentThread().interrupt(); // restore interrupted status
+  }
         action.clickEnglishButton();
         String message = action.getSelectedMessageToCheckLang();
         Assert.assertEquals(SPECIAL_OFFER_TEXT, message);
